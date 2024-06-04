8bb48e2f603d0ce7882deae851600d5b7dff199b
NERO <mxzxro@gmail.com>
2024-06-05 04:28:31 +0900

    ✨ [NETFLIX] HomeVC & SignUpVC 기본 틀 구성

---
+++ AutoLayout/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewController.swift
+//  AutoLayout
+//
+//  Created by NERO on 6/4/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+    }
+}
+
+//MARK: - Configure
+extension HomeViewController {
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .black
+        
+    }
+}
---
+++ AutoLayout/SignUpViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignUpViewController.swift
+//  AutoLayout
+//
+//  Created by NERO on 6/4/24.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureHierarchy()
+        configureLayout()
+        configureUI()
+    }
+}
+
+//MARK: - Configure
+extension SignUpViewController {
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .black
+        
+    }
+}
