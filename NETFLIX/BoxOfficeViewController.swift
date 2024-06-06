5a340ee4be0b0d7e16cec211649df065ad3f5323
NERO <mxzxro@gmail.com>
2024-06-07 03:06:15 +0900

    ✨ [BoxOfficeVC] 기본 틀 구성

--- NETFLIX/BoxOfficeViewController.swift
+++ NETFLIX/BoxOfficeViewController.swift
@@ -11,6 +11,41 @@ class BoxOfficeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureHierarchy()
+        configureData()
+        configureLayout()
+        configureUI()
+        
+        settingNavigation()
+    }
+}
 
+//MARK: - Configure
+extension BoxOfficeViewController {
+    func configureHierarchy() {
+        
+    }
+    
+    func configureData() {
+        
+    }
+}
+
+//MARK: - Configure UI
+extension BoxOfficeViewController {
+    func configureLayout() {
+        
+    }
+    
+    func configureUI() {
+        
+    }
+}
+
+//MARK: - Switching View
+extension BoxOfficeViewController {
+    func settingNavigation() {
+        navigationController?.navigationBar.isHidden = true
     }
 }
