a02c850e8838bbc8a38ef3be3d2e884fe9f4b981
NERO <mxzxro@gmail.com>
2024-06-06 18:50:53 +0900

    🔧 SceneDelegate에서 TabBarController 생성하여 rootVC 설정

---
+++ NETFLIX/BoxOfficeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BoxOfficeViewController.swift
+//  NETFLIX
+//
+//  Created by NERO on 6/5/24.
+//
+
+import UIKit
+
+class BoxOfficeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
--- NETFLIX/SceneDelegate.swift
+++ NETFLIX/SceneDelegate.swift
@@ -14,11 +14,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+
+        let tabBarController = UITabBarController()
         
-        let mainViewController = HomeViewController()
-        let navigationController = UINavigationController(rootViewController: mainViewController)
-        window?.rootViewController = navigationController
+        let homeVC = HomeViewController()
+        let boxOfficeVC = BoxOfficeViewController()
         
+        let firstNav = UINavigationController(rootViewController: homeVC)
+        let secondNav = UINavigationController(rootViewController: boxOfficeVC)
+        
+        firstNav.tabBarItem = UITabBarItem(title: "홈", image: UIImage(systemName: "house.fill"), tag: 0)
+        secondNav.tabBarItem = UITabBarItem(title: "박스오피스", image: UIImage(systemName: "movieclapper.fill"), tag: 1)
+        
+        tabBarController.tabBar.tintColor = .white
+        tabBarController.tabBar.unselectedItemTintColor = .gray
+        tabBarController.tabBar.backgroundColor = .secondaryLabel
+        tabBarController.setViewControllers([firstNav, secondNav], animated: true)
+        
+        window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
     }
 }
