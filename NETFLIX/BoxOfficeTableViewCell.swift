241f0e6122dcd0e29a64b181c8587b8820c326c0
NERO <mxzxro@gmail.com>
2024-06-10 07:55:15 +0900

    💄 [BoxOfficeCell] 레이아웃, UI 설정

--- NETFLIX/BoxOfficeTableViewCell.swift
+++ NETFLIX/BoxOfficeTableViewCell.swift
@@ -36,3 +36,48 @@ class BoxOfficeTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+//MARK: - Configure UI
+extension BoxOfficeTableViewCell {
+    func configureLayout() {
+        movieStackView.snp.makeConstraints {
+            $0.centerX.equalTo(contentView)
+            $0.centerY.equalTo(contentView)
+            $0.leading.trailing.equalTo(contentView.safeAreaLayoutGuide).inset(8)
+        }
+        
+        rankLabel.snp.makeConstraints {
+            $0.leading.equalTo(movieStackView)
+            $0.width.equalTo(35)
+            $0.height.equalTo(25)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(rankLabel.snp.trailing).offset(10)
+        }
+        
+        openDateLabel.snp.makeConstraints {
+            $0.width.equalTo(80)
+            $0.trailing.equalTo(movieStackView)
+        }
+    }
+    
+    func configureUI() {
+        self.selectionStyle = .none
+        
+        rankLabel.backgroundColor = .pointRed
+        rankLabel.textColor = .white
+        rankLabel.textAlignment = .center
+        rankLabel.layer.cornerRadius = 4
+        rankLabel.layer.masksToBounds = true
+        rankLabel.font = .systemFont(ofSize: 15, weight: .bold)
+        
+        titleLabel.textColor = .white
+        titleLabel.font = .systemFont(ofSize: 15, weight: .bold)
+        titleLabel.numberOfLines = 0
+
+        openDateLabel.textColor = .systemGray3
+        openDateLabel.textAlignment = .right
+        openDateLabel.font = .systemFont(ofSize: 13, weight: .medium)
+    }
+}
