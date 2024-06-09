ff9dc481f09d1ac5b010996a8b4a3d2a8edd4524
NERO <mxzxro@gmail.com>
2024-06-10 07:53:46 +0900

    ✨ [BoxOfficeCell] Property, Hierarchy 추가

--- NETFLIX/BoxOfficeTableViewCell.swift
+++ NETFLIX/BoxOfficeTableViewCell.swift
@@ -6,13 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class BoxOfficeTableViewCell: UITableViewCell {
     static let identifier = "BoxOfficeTableViewCell"
     
+    let rankLabel = UILabel()
+    let titleLabel = UILabel()
+    let openDateLabel = UILabel()
+    let movieStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        return stackView
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-
+        [rankLabel, titleLabel, openDateLabel].forEach { movieStackView.addArrangedSubview($0) }
+        contentView.addSubview(movieStackView)
+        
+        configureLayout()
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
