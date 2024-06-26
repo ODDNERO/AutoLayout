167af65fa669cc33598d2dbc03ad808f36cb0093
NERO <mxzxro@gmail.com>
2024-06-27 04:06:16 +0900

    ✨ TMDBList Dummy Data 추가

---
+++ NETFLIX/Network/Dummy.swift
@@ -0,0 +1,328 @@
+//
+//  Dummy.swift
+//  NETFLIX
+//
+//  Created by NERO on 6/26/24.
+//
+
+import Foundation
+
+class Dummy {
+    static let shared = Dummy()
+    private init() {}
+    
+    lazy var trend: [TMDBList] = [
+        TMDBList(
+            id: 1059572,
+            title: "신차원! 짱구는 못말려 더 무비 초능력 대결전 ~날아라 수제김밥~",
+            poster_path: "/39nnJB6453mDocXqy2RGc2zZ3wY.jpg",
+            backdrop_path: "/gjEiHrj8dSsgL25XbIxT9o5qO3u.jpg",
+            release_date: "2023-08-04",
+            vote_average: 6.9,
+            overview: "어느 날, 우주에서 날아온 검은 빛과 하얀 빛이 떡잎마을을 향해 떨어진다..."
+        ),
+        TMDBList(
+            id: 453575,
+            title: "짱구는 못말려 극장판: 습격!! 외계인 덩덩이",
+            poster_path: "/rqJtClna9UjgTBAWBGZ2UzuEdye.jpg",
+            backdrop_path: "/rPfjyws2DDm5lPPj5OQ5HjPDC8Q.jpg",
+            release_date: "2017-04-15",
+            vote_average: 7.3,
+            overview: "짱구의 집에 불시착한 UFO의 주인 외계인 ‘덩덩이'. '나스파디’ 별에서 온 ‘덩덩이’는 무시무시한 ‘꼬마꼬마 파워’로..."
+        ),
+        TMDBList(
+            id: 128883,
+            title: "짱구는 못말려 극장판: 전설을 부르는 춤을 춰라, 아미고!",
+            poster_path: "/3NyXfJqLW5AAXH4ii8Q62w0wU3c.jpg",
+            backdrop_path: "/jUKn2iQK2pd2XNM1dhcP7Gb991b.jpg",
+            release_date: "2006-04-15",
+            vote_average: 7.6,
+            overview: "삼바 리듬을 좋아하는 가짜 인간들이 나타나 진짜를 납치해버리고 자신들이 진짜인 것처럼 행동하기 시작한다..."
+        ),
+        TMDBList(
+            id: 37280,
+            title: "짱구는 못말려 극장판: 어른 제국의 역습",
+            poster_path: "/dUBxeDBAtr8kSIDDPD86aWqmHFr.jpg",
+            backdrop_path: "/tMu1ceUmscHoOJFCkz3HmTRBAg3.jpg",
+            release_date: "2001-04-21",
+            vote_average: 8.363,
+            overview: "떡잎마을에 20세기 박물관이 생기자 어른들은 그곳에 푹 빠져 헤어 나오지 못한다..."
+        ),
+        TMDBList(
+            id: 117087,
+            title: "짱구는 못말려 극장판: 폭풍을 부르는 정글",
+            poster_path: "/nfkk0PgdU6bUoLdh0Vnlc9nbOaG.jpg",
+            backdrop_path: "/tmj4cc8LKfAwxnF32M6fQk67tkK.jpg",
+            release_date: "2000-04-22",
+            vote_average: 8.0,
+            overview: "짱구는 호화유람선 여행에서 액션가면 시사회가 있다는 광고를 보고 부모님과 함께 남해섬으로 여행을 떠난다..."
+        ),
+        TMDBList(
+            id: 371236,
+            title: "짱구는 못말려 극장판: 나의 이사 이야기 선인장 대습격",
+            poster_path: "/a7apSmg36MmApRjGKPp4WQGBLpo.jpg",
+            backdrop_path: "/mBASjOb6DecQGZGPvnPlhOgnAUj.jpg",
+            release_date: "2015-04-18",
+            vote_average: 7.0,
+            overview: "안녕… 떡잎마을… 갑자기 멕시코 지사로 발령이 난 짱구아빠..."
+        ),
+        TMDBList(
+            id: 103124,
+            title: "짱구는 못말려 극장판: 태풍을 부르는 영광의 불고기 로드",
+            poster_path: "/kgWzcGZ0WqOwm8pSnEJjK5depu6.jpg",
+            backdrop_path: "/5xS4i5rC0NpzDrBMYoA3Ij0sGtF.jpg",
+            release_date: "2003-04-19",
+            vote_average: 7.1,
+            overview: "아침밥이 부실하자 생떼를 쓰며 불만을 토로하는 식구들에게 저녁에는 비싼 불고기를 해먹기로 약속한 엄마..."
+        ),
+        TMDBList(
+            id: 955666,
+            title: "짱구는 못말려 극장판: 동물소환 닌자 배꼽수비대",
+            poster_path: "/4DKjTt6VodfMRZ1Xs77peiQQXmY.jpg",
+            backdrop_path: "/zlHeQg9OTYED2Gm4ZErdvElsUFg.jpg",
+            release_date: "2022-04-22",
+            vote_average: 7.1,
+            overview: "어느 날 ‘짱구‘와 동갑내기인 5살 ‘진구’를 데리고, 짱구 가족을 찾아온 수상한 여성..."
+        ),
+        TMDBList(
+            id: 109526,
+            title: "짱구는 못말려 극장판: 부리부리 왕국의 보물",
+            poster_path: "/9djKSjGogao0ROefsa2YTeNy2w3.jpg",
+            backdrop_path: "/gU6PGO7u5BWYrsaZQ6UuTNG4xuj.jpg",
+            release_date: "1994-04-23",
+            vote_average: 6.9,
+            overview: "어느 날, 부리부리 왕국의 왕자 슨노케시는 이상한 자들에게 납치당한다..."
+        ),
+        TMDBList(
+            id: 163772,
+            title: "짱구는 못말려 극장판: 태풍을 부르는 나와 우주의 프린세스",
+            poster_path: "/oinFV053uo4PFog8jdfI1t3VpmH.jpg",
+            backdrop_path: "/lAoaliIyDNDJnj1FGp8rmGdLtMq.jpg",
+            release_date: "2012-04-13",
+            vote_average: 6.8,
+            overview: "오빠 따위는 당장 때려치우고 싶은 짱구에게 닥친 사상 최대 위기의 순간..."
+        ),
+        TMDBList(
+            id: 117352,
+            title: "짱구는 못말려 극장판: 초시공! 태풍을 부르는 나의 신부",
+            poster_path: "/4Nsau9uAIUBwATCF0dcM1Z7WUe9.jpg",
+            backdrop_path: "/snDGwQ3fbfNiGwUfT7dVPQ4MTSV.jpg",
+            release_date: "2010-04-17",
+            vote_average: 7.815,
+            overview: "약혼녀 다미에게 '5살의 나를 데려와줘!' 라는 말을 남기고 돌이 되어버린 어른 짱구..."
+        ),
+        TMDBList(
+            id: 795564,
+            title: "짱구는 못말려 극장판: 수수께끼! 꽃피는 천하떡잎학교",
+            poster_path: "/sqAm04lTqq5ZBWENlpV25ITAiVw.jpg",
+            backdrop_path: "/jQdD4cCoOFimwgeCYWh8jY7xBVb.jpg",
+            release_date: "2021-04-23",
+            vote_average: 7.7,
+            overview: "명문 엘리트 양성학교에 체험 입학하게 된 짱구와 친구들..."
+        ),
+        TMDBList(
+            id: 128868,
+            title: "짱구는 못말려 극장판: 액션가면 VS 그래그래 마왕",
+            poster_path: "/qP1JgbTpLITqXQM7qyaucdSNAxo.jpg",
+            backdrop_path: "/iGu27QGQtquhweyxXoVK4N6FkHs.jpg",
+            release_date: "1993-07-24",
+            vote_average: 7.1,
+            overview: "액션가면 VS 그래그래 마왕의 대결에 짱구는 완전히 매료된다..."
+        ),
+        TMDBList(
+            id: 38383,
+            title: "짱구는 못말려 극장판: 암흑 마왕 대추적",
+            poster_path: "/A7iqpXGH7eFDfHEowJ68VN0mEK5.jpg",
+            backdrop_path: "/pug99wOi6pAWl1Us3hMNa0mbFF6.jpg",
+            release_date: "1997-04-19",
+            vote_average: 6.7,
+            overview: "액션가면의 팬인 짱구는 TV를 통해 이상한 할아버지로부터..."
+        ),
+        TMDBList(
+            id: 171328,
+            title: "짱구는 못말려 극장판: 어른제국의 역습",
+            poster_path: "/pIApDZScdv1znRLD7YVbSlp11yz.jpg",
+            backdrop_path: "/qsZ6fNyMWboor0nWkIu4r2PZavz.jpg",
+            release_date: "2001-04-21",
+            vote_average: 8.363,
+            overview: "시공을 초월한 마을에 어른들이 빠지면서 벌어지는 사건..."
+        )
+    ]
+    
+    lazy var similar: [TMDBList] = [
+        TMDBList(
+            id: 38757,
+            title: "라푼젤",
+            poster_path: "/sT1vLH38iaezVgoFqmaiihbnTUy.jpg",
+            backdrop_path: "/cWczNud8Y8i8ab0Z4bxos4myWYO.jpg",
+            release_date: "2010-11-24",
+            vote_average: 7.607,
+            overview: "장장 18년을 탑 안에서만 지낸 끈기 만점의 소녀 라푼젤. 어느 날 자신의 탑에 침입한 왕국 최고의 대도를 한 방에 때려잡고, 그를 협박해 꿈에도 그리던 집 밖으로의 모험을 단행한다. 과잉보호 모친의 영향으로 세상을 험난한 곳으로만 상상하던 라푼젤 앞에 왕실 경비마 맥시머스의 추격, 라이더에게 복수의 칼날을 가는 스태빙턴 형제의 위협, 라푼젤의 가짜 엄마 고델의 무서운 음모 등이 얽혀 점점 흥미진진한 사건들이 터지기 시작한다. 그러나 세상물정 모르는 라푼젤은 자신 앞에 펼쳐진 스릴 넘치는 세상을 맘껏 즐기는데..."
+        ),
+        TMDBList(
+            id: 269149,
+            title: "주토피아",
+            poster_path: "/fZcab1yiKXsjx3S8D4KRHZsnMGC.jpg",
+            backdrop_path: "/p2fRZzxla6NoRbIH2KOZq0gHb5S.jpg",
+            release_date: "2016-02-11",
+            vote_average: 7.747,
+            overview: "어릴 적부터 경찰이 꿈이었던 토끼 주디 홉스는 주변의 만류에도 불구하고 경찰학교에 들어가 당당히 수석으로 졸업한다. 온갖 동물들이 모여 살며 교양 있고 세련된 라이프 스타일을 주도하는 대도시 주토피아에 자원한 주디는 의욕을 안고 출근하지만, 상사는 작은 토끼라는 이유로 주차관리 같은 소일거리만 시킨다. 따분하게 업무를 보던 주디는 아이스크림 불법 판매를 일삼는 사기꾼 여우 닉 와일드를 알게 되고, 그와 함께  48시간 안에 주토피아에서 벌어지고 있는 연쇄 실종사건을 추적해야만 하는데..."
+        ),
+        TMDBList(
+            id: 109451,
+            title: "하늘에서 음식이 내린다면 2",
+            poster_path: "/wbCPYQCwgBpnS5ZSvV5UkFAE5fk.jpg",
+            backdrop_path: "/vWriOBVlgW0XWdgbOiijhyLW7B8.jpg",
+            release_date: "2013-09-26",
+            vote_average: 6.44,
+            overview: "엉뚱한 발명가 플린트가 또 일을 냈다! 물로 거대한 음식을 만들어내던 플린트의 기계가 이번엔 살아 있는 푸드 몬스터를 만들어 낸 것. 햄버거미, 악어타코, 메론염소, 수박코끼리 등등 푸드 몬스터들이 사람들을 먹어버리기 전에 어서 빨리 그들을 막아야 해! 플린트와 친구들이 함께하는 짜릿한 모험이 시작됐다. 자, 이제 다 함께 꿀꺽퐁당 섬으로 출~발!"
+        ),
+        TMDBList(
+            id: 177572,
+            title: "빅 히어로",
+            poster_path: "/b7pVujY0zLImUyCg1w7YNmh3tLf.jpg",
+            backdrop_path: "/t3DdoWhv8xvk6K1PabKjMJGTwwt.jpg",
+            release_date: "2014-10-24",
+            vote_average: 7.734,
+            overview: "천재 공학도 테디가 개발한 힐링로봇 베이맥스. 테디의 동생이자 로봇 전문가인 히로는 도시가 파괴될 위기에 처하자 베이맥스를 슈퍼히어로로 업그레이드 하는데... 과연 이들은 도시의 위험을 막아낼 수 있을까?"
+        ),
+        TMDBList(
+            id: 93456,
+            title: "슈퍼배드 2",
+            poster_path: "/4agimSlOtKc1810dY1MSVivPHC4.jpg",
+            backdrop_path: "/fOipappvgVtUbbHOtmCkHzcwJjC.jpg",
+            release_date: "2013-06-26",
+            vote_average: 6.925,
+            overview: "슈퍼 악당에서 딸바보 슈퍼 대디로 180도 변신한 그루는  수다스럽고 사랑스러운 세 딸 마고, 에디스, 아그네스와 함께 행복한 삶을 살고 있다.  어느 날 거대한 자석에 의해 남극의 비밀연구소가 사라지는 사건이 발생한다. 알고보니 이 연구소는 생물을 야수로 만드는 약물을 개발하고 있었고, 악당퇴치연맹은 이 짓을 벌인 악당을 잡기 위해 그루에게 조언을 구한다. 세 딸과 평화로운 시간을 보내던 그루는 비밀요원 루시와 함께 위장 쿠키 가게를 차려 악당을 찾고, 이 과정에서 루시에게 마음을 뺏기기 시작한다. 한편 정체 모를 악당은 미니언들을 납치해 괴물로 변신시켜 세계를 정복하려 하지만 그루는 이 사실도 모른 채 루시와 딸들에게만 신경을 쓰는데..."
+        ),
+        TMDBList(
+            id: 12,
+            title: "니모를 찾아서",
+            poster_path: "/9ViCYfZ0whpwtKbM2WJP5PfsG2x.jpg",
+            backdrop_path: "/eCynaAOgYYiw5yN5lBwz3IxqvaW.jpg",
+            release_date: "2003-05-30",
+            vote_average: 7.822,
+            overview: "광대어 말린은 아내 코랄과 2세들의 부화를 기다리던 중 상어의 습격을 받는다. 알을 보호하려던 아내는 행방불명되고, 유일하게 살아남은 아기에게 말린은 니모라는 이름을 붙인다. 사건 이후 큰 바다를 무조건 겁내게 된 말린은 니모를 과보호한다. 하지만 니모는 등교 첫날 잠수부에게 납치돼 시드니에 있는 치과의사의 수족관에 끌려가고, 슬픔으로 혼비백산한 아빠 말린은 평소의 심약함을 잊고니모를 찾아나선다. 단기기억상실증을 지닌 명랑한 물고기 도리의 도움으로 상어, 심해어, 해파리의 위협을 뚫고 동호주 해류로 향하는 동안, 니모는 수족관의 새 친구들과 탈출을 모의한다."
+        ),
+        TMDBList(
+            id: 82690,
+            title: "주먹왕 랄프",
+            poster_path: "/A6jGfyW7yzTpXkRJovUhe2IanKu.jpg",
+            backdrop_path: "/ziC23LkMYj8gToQQYQGWSGJCLNF.jpg",
+            release_date: "2012-11-01",
+            vote_average: 7.338,
+            overview: "8비트 게임 ‘다고쳐 펠릭스’에서 건물을 부수는 악당 주먹왕 랄프. 30년째 매일같이 건물을 부수며 직업에 충실해왔지만, 악당이라는 이유로 누구도 그를 좋아하지 않는다. 모두에게 인정 받는 영웅이 되고 싶은 마음에 급기야 자기 게임을 이탈하여 다른 게임으로 들어가는 랄프! 슈팅게임 ‘히어로즈 듀티’를 거쳐 레이싱 게임 ‘슈가 러시’에 불시착하는 랄프는 과연 게임 세계의 새로운 영웅이 될 수 있을 것인가! 그리고 랄프가 떠난 후 고장 딱지가 붙은 ‘다고쳐 펠릭스’ 게임은 오락실에서 퇴출될 위기를 넘길 수 있을까?"
+        ),
+        TMDBList(
+            id: 62211,
+            title: "몬스터 대학교",
+            poster_path: "/7K5BqOlgDOs2DI2PKDUGlMtYheL.jpg",
+            backdrop_path: "/hmAOMwVeJfdWXgK1Ikyl2eYkE99.jpg",
+            release_date: "2013-06-19",
+            vote_average: 7.038,
+            overview: "매사에 열심이지만 이론만 빠삭한 마이크와 무늬만 엄친아인 허세 몬스터 설리는 몬스터 주식회사 입사의 꿈을 안고 취업 100% 보장 특성화 대학 몬스터 대학교에 입학한다. 하지만 성격도 재능도 정반대인 둘은 첫날부터 삐걱거리며 급기야는 개교이래 최악의 라이벌이 되고야 만다. 하지만 그들이 힘을 합쳐야 하는 순간이 찾아온다. 부푼 꿈을 안고 몬스터 대학교에 진학한 그들이 전공 퇴출 위기에 몰리면서, 교내 겁주기 대회에 팀을 이뤄 출전하기로 한 것. 그렇게 그들은 엄한 학장의 경계 속에 최약체 팀으로 대회에 나선다. 하지만 초반의 실수를 딛고 이내 승승장구하며 교내의 영웅으로 떠오르기 시작한다."
+        ),
+        TMDBList(
+            id: 14160,
+            title: "업",
+            poster_path: "/lL2WzkjLZP0RkPgUhybBjb9x9SL.jpg",
+            backdrop_path: "/hGGC9gKo7CFE3fW07RA587e5kol.jpg",
+            release_date: "2009-05-28",
+            vote_average: 7.959,
+            overview: "평생 모험을 꿈꿔 왔던 칼 할아버지는 재개발 공사 현장의 인부를 실수로 다치게 하고 집을 빼앗길 위기에 처한다. 그 동안 미뤄 왔던, 먼저 세상을 떠난 아내와의 약속을 지키기 위해 그는 수천 개의 풍선을 매달아 집을 통째로 남아메리카로 날려 버린다. 하지만 이 위대한 모험에 초대 받지 않은 불청객이 있었으니, 바로 황야의 어린이 탐험가 러셀. 우여곡절 끝에 도착한 남미에서 칼은 어린 시절의 우상이었던 위대한 모험가 찰스 먼츠를 만나게 되는데... 그들은 과연 남미의 잃어버린 세계에서 사라져 버린 꿈과 희망, 행복을 다시 찾을 수 있을까?"
+        ),
+        TMDBList(
+            id: 2062,
+            title: "라따뚜이",
+            poster_path: "/zFah4LNeWw68y7RRGi20CQlGByY.jpg",
+            backdrop_path: "/xgDj56UWyeWQcxQ44f5A3RTWuSs.jpg",
+            release_date: "2007-06-21",
+            vote_average: 7.821,
+            overview: "절대미각, 빠른 손놀림, 끓어 넘치는 열정의 소유자 레미. 프랑스 최고의 요리사를 꿈꾸는 그에게 단 한가지 약점이 있었으니, 바로 주방 퇴치대상 1호인 생쥐라는 것. 어느 날 하수구에서 길을 잃은 레미는 파리의 별 다섯개짜리 최고급 레스토랑에 떨어진다. 보글거리는 수프, 둑닥둑닥 도마소리, 향긋한 허브 내음에 식욕이 아닌 요리욕이 북받친 레미의 작은 심장은 콩닥콩닥 뛰기 시작한다. 끝내 주방으로 들어가는 레미는 요리에 열중하다 견습생 링귀니에게 딱 걸리고 만다. 해고위기에 처해있던 링귀니는 레미의 재능을 한눈에 알아보고 의기투합을 제안하는데..."
+        ),
+        TMDBList(
+            id: 10681,
+            title: "WALL·E",
+            poster_path: "/qYMQMuDHXeEDoHbGFkvPATG3eC0.jpg",
+            backdrop_path: "/fK5ssgvtI43z19FoWigdlqgpLRE.jpg",
+            release_date: "2008-06-22",
+            vote_average: 8.09,
+            overview: "텅 빈 지구에 홀로 남아 수백 년이란 시간을 외롭게 일만 하며 보내던 월-E (WALL-E: Waste Allocation Load Lifter Earth-Class, 지구 폐기물 수거-처리용 로봇). 그런 그가 매력적인 탐사 로봇 ‘이브’와 마주친 순간, 잡동사니 수집만이 낙이던 인생에도 소중한 목표가 생긴다. 이브는 지구의 미래를 결정할 열쇠가 우연히 월-E의 손에 들어간 사실을 알게 되고, 고향별로 돌아갈 날만 애타게 기다리는 인간들에게 이를 보고하기 위해 서둘러 우주로 향한다. 한편 월-E는 이브를 뒤쫓아 은하를 가로지르며, 스크린 사상 가장 짜릿한 상상이 넘치는 어드벤처를 선사한다. 이제껏 꿈에서도 볼 수 없었던 미래 세계를 배경으로 우주에서 펼쳐지는 월-E의 환상적인 모험! 애완용 바퀴벌레, 용맹스럽지만 어딘가 나사가 빠진 듯한 사회 부적응 로봇 군단 등 일련의 유쾌한 캐릭터들이 여기에 동참한다."
+        )
+    ]
+    
+    lazy var recommendations: [TMDBList] = [
+        TMDBList(
+            id: 283995,
+            title: "가디언즈 오브 갤럭시 Vol. 2",
+            poster_path: "/uDuwGhQgffd5GQ695oUfy5oZGqP.jpg",
+            backdrop_path: "/n3gb8oqP9BeRCUsCmZVaT5eltPH.jpg",
+            release_date: "2017-04-19",
+            vote_average: 7.621,
+            overview: "타노스에 맞서 은하계를 구하고 최고의 해결사로 등극한 스타로드 일행. 하지만 소버린 행성의 여사제 아이샤를 돕던 중 옛버릇을 고치지 못하고 행성의 배터리를 훔쳤다가 소버린 종족에 쫓기는 신세가 된다. 그 과정에서 피터의 아버지 에고가 그들을 돕는다. 아픈 엄마와 어린 자신을 버리고 떠난 아버지에 대한 원망이 컸던 피터는 에고의 행성에서 아버지의 능력을 확인하곤 그 힘에 이끌린다. 하지만 가모라는 에고의 행성에서 이상한 기운을 감지한다. 한편 반란군에 의해 위기에 처한 욘두는 로켓과 그루트의 도움으로 목숨을 구한다."
+        ),
+        TMDBList(
+            id: 99861,
+            title: "어벤져스: 에이지 오브 울트론",
+            poster_path: "/sTUiA9zY6O4qFlmRC69nG6dngTh.jpg",
+            backdrop_path: "/6YwkGolwdOMNpbTOmLjoehlVWs5.jpg",
+            release_date: "2015-04-22",
+            vote_average: 7.273,
+            overview: "토니 스타크는 뉴욕전쟁 때와 같은 사태가 벌어지지 않도록 한때 가동하려다 중단된 휴면 상태의 평화 유지 프로그램을 작동 시키려 한다. 배너 박사와 함께 지구를 지킬 최강의 인공지능 울트론을 탄생시키게 되지만, 울트론은 예상과 다르게 지배를 벗어나 폭주하기 시작하고 어벤져스는 지구의 운명이 걸린 거대한 시험대에 오르게 된다. 울트론이 자신을 복제해 위협을 가하자 이를 저지하기 위해 아이언맨, 캡틴 아메리카, 토르, 헐크, 블랙위도우, 호크아이 등으로 구성된 어벤져스와 새로 합류하게 되는 퀵 실버, 스칼렛 위치 남매와 불안한 동맹을 맺는다."
+        ),
+        TMDBList(
+            id: 100402,
+            title: "캡틴 아메리카: 윈터 솔져",
+            poster_path: "/zcSTD8KeGeGDwlrZTq23RN4B6sP.jpg",
+            backdrop_path: "/1RWLMyC9KcFfcaoViMiJGSSZzzr.jpg",
+            release_date: "2014-03-20",
+            vote_average: 7.7,
+            overview: "스티브 로저스는 외계의 뉴욕 침공 사건이 있은 뒤 워싱턴에서 쉴드 요원으로 살아가고 있다. 납치된 쉴드의 함선을 구출하는 작전을 펼치던 중 스티브는 닉 퓨리 국장과 블랙 위도우가 쉴드의 정보를 무단으로 수집하고 있다는 것을 알게 된다. 그리고 얼마 뒤 닉 퓨리는 윈터 솔져의 공격으로 눈을 감는다. 블랙 위도우와 함께 닉 퓨리가 남긴 USB를 분석하며 진실을 추적하던 스티브는 적이 쉴드 내부에 존재한다는 사실을 알게 된다. 설상가상 윈터 솔져가 캡틴 아메리카의 단짝 친구 버키라는 사실이 드러나는데..."
+        ),
+        TMDBList(
+            id: 127585,
+            title: "엑스맨: 데이즈 오브 퓨처 패스트",
+            poster_path: "/zm5zanw86TX2T7qJNTKilL3x3S8.jpg",
+            backdrop_path: "/fctQU5MoXgJ5pNMljFzlEFXwfSu.jpg",
+            release_date: "2014-05-15",
+            vote_average: 7.527,
+            overview: "돌연변이들을 멸종시키기 위해 제작된 로봇 센티넬이 인간들까지 모조리 말살하는 2023년. 인류의 종말이 코앞에 닥친 상황에서 프로페서X와 매그니토는 과거를 통해 미래의 운명을 바꾸고자 한다. 울버린은 트라스크가 센티넬을 개발하는 것을 막기 위해 1973년으로 보내진다. 1973년의 찰스는 절망에 빠진 채 행크 맥코이와 함께 은둔 생활을 하고 있고, 에릭은 케네디 대통령 암살 용의자로 붙잡혀 펜타곤 지하 깊숙한 곳에 위치한 수용소에 갇혀 있다."
+        ),
+        TMDBList(
+            id: 240832,
+            title: "루시",
+            poster_path: "/dHPhwkoDDslf2SFzccRG17Y9f2K.jpg",
+            backdrop_path: "/qpBZE42SBLMJKNSxgW5wGc8Ip9e.jpg",
+            release_date: "2014-07-25",
+            vote_average: 6.448,
+            overview: "평범한 삶을 살던 여자 루시는 어느 날 지하세계에서 극악무도하기로 유명한 미스터 장에게 납치되어, 몸 속에 강력한 합성 약물을 넣은 채 강제로 운반하게 된다. 다른 운반책들과 같이 끌려가던 루시는 갑작스런 외부의 충격으로 인해 몸 속 약물이 체내로 퍼지게 되면서, 그녀 안의 모든 감각이 깨어나기 시작하는데…"
+        ),
+        TMDBList(
+            id: 324857,
+            title: "스파이더맨: 홈 카밍",
+            poster_path: "/c24sv2weTHPsmDa7jEMN0m2P3RT.jpg",
+            backdrop_path: "/vc8bCGjdVp0UbMNLzHnHSLRbBWQ.jpg",
+            release_date: "2017-07-05",
+            vote_average: 7.411,
+            overview: "피터 파커는 앞에 고등학교에서의 일상적인 생활을 하는 데, 스파이더맨의 책임을 맡아야 했다. 하지만 곧 그의 라이프는 치명적이었고 그는 반드시에 악당 보존하고 라이프의 균형을 잡을 수 있을까?"
+        ),
+        TMDBList(
+            id: 299536,
+            title: "어벤져스: 인피니티 워",
+            poster_path: "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+            backdrop_path: "/lmZFxXgJE3vgrciwuDib0N8CfQo.jpg",
+            release_date: "2018-04-25",
+            vote_average: 8.329,
+            overview: "무한의 돌을 찾기 위해 우주를 쫓는 타노스는 히어로들이 가진 6개의 무한의 돌을 모두 모으려고 한다. 히어로들은 그의 음모를 막기 위해 음모를 꾸미는 노력을 하지만, 그의 영향을 피할 수 없다. 세상을 구할 수 있을까요?"
+        ),
+        TMDBList(
+            id: 284054,
+            title: "블랙 팬서",
+            poster_path: "/uxzzxijgPIY7slzFvMotPv8wjKA.jpg",
+            backdrop_path: "/6ELJEzQJ3Y45HczvreC3dg0GV5R.jpg",
+            release_date: "2018-02-13",
+            vote_average: 7.441,
+            overview: "폐허가 된 왕국 워칸다를 다스린 다는 티차라는 블랙 팬서는, 외부세계와 떨어져 있던 고향으로 돌아와 자신의 왕위를 계승한다. 그러나 그의 존재가 알려지고, 그는 자신의 신분과 연결되는 과거의 잘못들에 대해 직면하게 된다. 블랙 팬서는 그의 힘을 사용하여 그의 백성을 통합하고 그의 땅을 보호하기 위해 최선을 다해야 한다."
+        )
+    ]
+}
